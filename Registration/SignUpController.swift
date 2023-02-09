ed05a8641d570bb590428e138be2b1509d1dad57
yavuz yerer <yavuz.yererr@gmail.com>
2023-02-09 20:02:51 +0300

    START PROJECT

---
+++ Registration/SignUpController.swift
@@ -0,0 +1,16 @@
+//
+//  SignUpController.swift
+//  Registration
+//
+//  Created by Yavuz Yerer on 9.02.2023.
+//
+
+import UIKit
+
+class  SignUpController: UIViewController {
+    override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            view.backgroundColor = .white
+        }
+}
